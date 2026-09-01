14a0f77239bfac79bac0283516ef2716ac186605
kaiyichen124 <13756479686@163.com>
2026-09-01 23:33:24 +0800

    Connect frontend to Dify v15.3 workflow

---
+++ next/config.js
@@ -0,0 +1,6 @@
+window.VACCINE_AGENT_CONFIG = Object.freeze({
+  DIFY_ORIGIN: 'https://udify.app',
+  APP_CODE: 'EIt257DNWsWxndqa',
+  BACKEND_RELEASE: 'v15.3-safety-gates',
+  REQUEST_TIMEOUT_MS: 120000,
+});
