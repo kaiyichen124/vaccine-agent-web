cfe1959d78a7aef7a6dcad96441443c961b8fc5a
kaiyichen124 <13756479686@163.com>
2026-09-02 11:54:23 +0800

    Connect frontend to Dify v16.1 workflow

--- next/config.js
+++ next/config.js
@@ -1,6 +1,6 @@
 window.VACCINE_AGENT_CONFIG = Object.freeze({
   DIFY_ORIGIN: 'https://udify.app',
-  APP_CODE: 'sx0Oi9Sbw1s9exp4',
-  BACKEND_RELEASE: 'v16-deepseek-72',
+  APP_CODE: 'FbBricLjnWfDAp0V',
+  BACKEND_RELEASE: 'v16.1-deepseek-72',
   REQUEST_TIMEOUT_MS: 120000,
 });
