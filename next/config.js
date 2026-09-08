137fabc5090dd634f170b00f8c711c7c3cdb414e
kaiyichen124 <13756479686@163.com>
2026-09-08 12:06:01 +0800

    Connect frontend to v16.2 RAG workflow
    Use Dify workflow rUdrwRKlc7kPrPZ0 for the v16.2 RAG frontend.

--- next/config.js
+++ next/config.js
@@ -1,6 +1,6 @@
 window.VACCINE_AGENT_CONFIG = Object.freeze({
   DIFY_ORIGIN: 'https://udify.app',
-  APP_CODE: 'AAl9Ql2pafQujbUy',
+  APP_CODE: 'rUdrwRKlc7kPrPZ0',
   BACKEND_RELEASE: 'v16.2-rag-routing',
   REQUEST_TIMEOUT_MS: 120000,
   STREAM_IDLE_TIMEOUT_MS: 30000,
