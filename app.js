c8aec370e71d26464363123a601358f2a2f76505
kaiyichen124 <13756479686@163.com>
2026-09-09 20:00:48 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const DIFY_ORIGIN = 'https://udify.app';
-const APP_CODE = 'NBCkZ0LbVfbuzNla';
+const APP_CODE = 'l63CgP5Bp6mjZpRl';
 
 const form = document.querySelector('#case-form');
 const submitButton = document.querySelector('#submit-button');
