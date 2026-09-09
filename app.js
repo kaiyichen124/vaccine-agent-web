65de743e5743b6e04d01b8e052573e328f100d8e
kaiyichen124 <13756479686@163.com>
2026-09-09 11:59:09 +0800

    revert: restore v18 web workflow

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const DIFY_ORIGIN = 'https://udify.app';
-const APP_CODE = 'ufcX8MEBjSCCnpTM';
+const APP_CODE = 'NvRpaqjNOhAITsnZ';
 
 const form = document.querySelector('#case-form');
 const submitButton = document.querySelector('#submit-button');
