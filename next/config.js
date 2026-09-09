aecaa1bf59d3d2d246018540cefcc6a2ba68eb3c
kaiyichen124 <13756479686@163.com>
2026-09-09 17:36:41 +0800

    Add files via upload

--- next/config.js
+++ next/config.js
@@ -1,7 +1,7 @@
 window.VACCINE_AGENT_CONFIG = Object.freeze({
   DIFY_ORIGIN: 'https://udify.app',
   APP_CODE: 'l63CgP5Bp6mjZpRl',
-  BACKEND_RELEASE: 'v21.0-single-merge',
+  BACKEND_RELEASE: 'v21.1-state-locked',
   REQUEST_TIMEOUT_MS: 120000,
   STREAM_IDLE_TIMEOUT_MS: 30000,
 });
