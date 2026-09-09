c30f0b13c23875d50cdecf89580a14cfed9caf26
kaiyichen124 <13756479686@163.com>
2026-09-09 17:43:14 +0800

    Add files via upload

--- next/config.js
+++ next/config.js
@@ -1,6 +1,6 @@
 window.VACCINE_AGENT_CONFIG = Object.freeze({
   DIFY_ORIGIN: 'https://udify.app',
-  APP_CODE: 'l63CgP5Bp6mjZpRl',
+  APP_CODE: '9SYptxEN4NSBx3eA',
   BACKEND_RELEASE: 'v21.1-state-locked',
   REQUEST_TIMEOUT_MS: 120000,
   STREAM_IDLE_TIMEOUT_MS: 30000,
