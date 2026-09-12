d530b4b824552285351eb9da5707e7fe58309cc5
kaiyichen124 <13756479686@163.com>
2026-09-12 11:48:01 +0800

    Add v22 fresh route: config.js

---
+++ v22/config.js
@@ -0,0 +1,7 @@
+window.VACCINE_AGENT_CONFIG = Object.freeze({
+  DIFY_ORIGIN: 'https://udify.app',
+  APP_CODE: 'ImiVokl6fFSznXgg',
+  BACKEND_RELEASE: 'v22.0-meeting-revision',
+  REQUEST_TIMEOUT_MS: 120000,
+  STREAM_IDLE_TIMEOUT_MS: 30000,
+});
